4f7783b4413921776b81bcfab32c8ee060d9dcf6
Simon Maina <simonmaina4927@gmail.com>
2026-02-11 10:27:29 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -4,10 +4,10 @@
 import { FirebaseOptions } from 'firebase/app';
 
 export const firebaseConfig: FirebaseOptions = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  projectId: 'studio-8029784041-8d4c8',
+  appId: '1:793868989809:web:be2e9daa25088a7f6572d0',
+  apiKey: 'AIzaSyB3dhwwvoaxkz_RQzBJlL-RovnFu8RTYJc',
+  authDomain: 'studio-8029784041-8d4c8.firebaseapp.com',
+  measurementId: '',
+  messagingSenderId: '793868989809',
 };
